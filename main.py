d1f0696ba5d2da07fc51ed39694fa0cd3731968f
MikhailOnufrienko <ghast13@yandex.ru>
2023-11-04 20:38:26 +0400

    Add Dockerfile and docker-compose.yaml.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
@@ -39,12 +38,3 @@ def startup() -> None:
 @app.on_event('shutdown')
 def shutdown() -> None:
     postgres.close()
-
-
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='127.0.0.1',
-        port=8001, 
-        reload=True
-    )
